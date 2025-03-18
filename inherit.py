7697f6ae6154cb6e33c04ebbda3978f8459961a4
Kuroyukihime000 <mishutka2282@tutamail.com>
2025-03-18 14:23:25 +0300

    2

--- inherit.py
+++ inherit.py
@@ -2,33 +2,32 @@
 
 class Employee:
     emp_count = 0
-    def __init__(self, name, position, salary, worked_days):
+    def __init__(self, name, position, procent_position, salary, worked_days):
         self.name = name
         self.position = position
         self.salary = salary
         self.worked_days = worked_days
+        self.procent_position = procent_position
 
     def info1(self):
-        print (f"Имя: {self.name}, Должность: {self.position}, Зарплата: {self.salary}")
+        print (f"Имя: {self.name}, Должность: {self.position}, Оклад: {self.salary}")
 
     def calc_salary1(self):
-        self.salary_all = self.position*self.salary*self.worked_days
-        print(f" salary = {self.salary_all}")
+        self.salary_all = self.procent_position*self.salary*self.worked_days
+        print(f" Зарплата = {self.salary_all}")
 
     
 class Manager(Employee):
-    def __init__(self, name, position, salary, worked_days, department, num_employees):
-        super().__init__(name, position, salary, worked_days)
+    def __init__(self, name, position, procent_position, salary, worked_days, department, num_employees):
+        super().__init__(name, position, procent_position, salary, worked_days)
         self.department = department 
         self.num_employees = num_employees
 
     def info2(self):
-        super().info1()
-        print(f"Имя: {self.name}, Должность: {self.position}, Зарплата: {self.salary}, department : {self.department}, Кол-во работников : {self.num_employees}")
+        print(f"Имя: {self.name}, Должность: {self.position}, Оклад: {self.salary}, department : {self.department}, Кол-во работников : {self.num_employees}")
     
     def calc_salary2(self):
-        super().calc_salary()
-        self.calc_salary0 = self.position*self.salary*self.worked_days + self.position*self.salary*self.worked_days*
+        self.calc_salary0 = self.procent_position*self.salary*self.worked_days + self.procent_position*self.salary*self.worked_days*(1/self.num_employees)
         print(f"Зарплата : {self.calc_salary0}")
         
 
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from inherit import Employee
 from inherit import Manager
 
 def main():
-    first = Employee('Иван Иванович', 'Менеджер', 50000, 24)
-    second = Manager('Иван Иванович', 'Менеджер', 50000, 24,'Отдел продаж', 50)
+    first = Employee('Иван Иванович', 'Менеджер', 2,  50000, 24)
+    second = Manager('Иван Иванович', 'Менеджер', 2 , 50000, 24,'Отдел продаж', 50)
     first.info1()
     first.calc_salary1()
     second.info2()
-    second.calc_salary2
+    second.calc_salary2()
 
 if __name__ == '__main__': #проверяем на прямой запуск
     main() #запускаем функцию
